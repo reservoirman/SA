0cee15dd57b63c4c0b11b9bfcc0881aaebf7ddaa
Ten-Seng Guh <tg2458@columbia.edu>
2014-09-21 18:16:00 -0700

    testing out getopt

--- tg2458.0/objlist.c
+++ tg2458.0/objlist.c
@@ -4,22 +4,47 @@
 #include <stdlib.h>
 
 
-int main (int argv, char **argc)
+int main (int argc, char **argv)
 {
 	//if the user supplied all of the arguments
-	if (argv == 4)
+	if (argc == 4)
 	{
 		//print them out
-		printf("Listing objects including their metadata for user %s.\n", argc[2]);	
+		printf("Listing objects including their metadata for user %s.\n", argv[2]);	
 	}
-	else if (argv == 3)
+	else if (argc == 3)
 	{
-		printf("Listing objects for user %s.\n", argc[2]);	
+		printf("Listing objects for user %s.\n", argv[2]);	
 	}
 	//otherwise, remind the user that he needs to do that
 	else
 	{
 		printf("Please enter user name and object name.\n");
+	}
+
+	int opt;
+
+	while ((opt = getopt(argc, argv, ":if:lr")) != -1)
+	{
+		switch (opt)
+		{
+			case 'f':
+			printf("filename: %s\n", optarg);
+			break;
+			case ':':
+			printf("Option needs a value\n");
+			break;
+			case '?':
+			printf("Unknown option: %c\n", optopt);
+			break;
+
+		}
+
+		for (; optind < argc; optind++)
+		{
+			printf("argument: %s\n", argv[optind]);
+		}
+
 	}
 
 }
